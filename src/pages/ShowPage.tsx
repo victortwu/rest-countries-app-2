16ee252ecaf679e144027b6927a9266b67697439
victortwu <victor607@gmail.com>
2021-11-25 00:09:22 -0800

    started styling the show page

--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -7,39 +7,53 @@ import style from '../cssModules/showPage.module.css'
 interface Props {
     countries: any[]
     countryCodeObj: any
-    
+
 }
 
 const ShowPage: FC<Props> = ( { countries, countryCodeObj } ) => {
 
     let params = useParams()
     let navigate = useNavigate()
-   
+
     const [countryParam, setCountryParam] = useState<string | undefined>(params.country)
 
     useEffect(()=> {
         setCountryParam(countryParam)
     }, [countryParam])
-  
+
     return(
         <div className={style.container}>
-            <h1>Show Page</h1>
-            <div>{countries.map(nation=> {
+
+            {countries.map(nation=> {
                 if ( nation.name.common === countryParam ) {
-                 
-                    return <div key={uuidv4()}>
-                                <h3>{nation.name.common}, Capital: {nation.capital?.map((cap:string)=> {return <span key={uuidv4()}>{cap}</span>})}</h3>
-                                <div>
-                                    { 
-                                        nation.borders?.map((code: string, i: number)=> {
-                                            return <button onClick={()=> setCountryParam(countryCodeObj[code])} key={uuidv4()}>{countryCodeObj[code]}</button>
-                                        })
-                                    }
+
+                    return <div className={style.contentDiv} key={uuidv4()}>
+
+                                <div className={style.containerLeft}>
+                                    <button onClick={()=> navigate(-1)}>BACK</button>
+                                    <h1>{nation.name.common}</h1>
+                                    <div className={style.imgContainer}>
+                                        <img src={nation.flags.svg} alt='flag'/>
+                                    </div>
+                                    <h4>Capital: {nation.capital?.map((cap:string)=> {return <span key={uuidv4()}>{cap}</span>})}</h4>
+                                    <p>Population: {nation.population.toLocaleString('en-US')}</p>
+
+                                </div>
+
+                                <div className={style.containerRight}>
+                                    <div className={style.bordersDiv}>
+                                        {
+                                            nation.borders?.map((code: string, i: number)=> {
+                                                return <button onClick={()=> setCountryParam(countryCodeObj[code])} key={uuidv4()}>{countryCodeObj[code]}</button>
+                                            })
+                                        }
+                                    </div>
                                 </div>
                             </div>
+
                 }
-            })}</div>
-        <button onClick={()=> navigate(-1)}>BACK</button>
+            })}
+
         </div>
     )
 }
