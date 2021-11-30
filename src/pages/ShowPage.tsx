d5610c7503685916453c251450ce13ddbb3bdda0
victortwu <victor607@gmail.com>
2021-11-29 21:24:15 -0800

    refactored to make api call to own server

--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -16,52 +16,30 @@ const ShowPage: FC<Props> = ( { countries, countryCodeObj } ) => {
     let navigate = useNavigate()
 
     const [countryParam, setCountryParam] = useState<string | undefined>(params.country)
-    const [latLong, setLatLong] = useState([])
+   
 
-    const baseUrl = 'http://dataservice.accuweather.com'
-    const apiKey = process.env.REACT_APP_WEATHERAPIKEY
-
-    const getWeatherInfo = (key: string) => {
-        fetch(`${baseUrl}/currentconditions/v1/${key}?apikey=${apiKey}`)
-        .then(res=> {
-            if(!res.ok) {
-                throw Error('Cannot get the weather')
-            }
-            return res.json()
-        })
-        .then(data=> {
-            console.log(data)
-        })
-        .catch(err=> {
-            console.error(err.message)
-        })
-    }
+    const baseUrl = 'http://localhost:5000'
+   
+    // accuweather fetch call to own backend, only 50 free calls a day
     
-    const getKeyByGeo = (longLat: any[] ) => {
-        console.log(longLat[0])
-        console.log(longLat[1])
-        fetch(`${baseUrl}/locations/v1/cities/geoposition/search?apikey=${apiKey}=${longLat[0]},${longLat[1]}`)
-        .then(res=> {
-            console.log(res)
-            if(!res.ok) {
-                throw Error('Cannot get location')
-            }
-            return res.json()
-        })
-        .then(data=> {
-            console.log(data)
-            //getWeatherInfo(data.key)
-        })
-        .catch(err=> {
-            console.error(err.message)
-        })
-    }
-
-    const getLatLong = () => {
+    const getWeather = () => {
         countries.map(nation=> {
             if ( nation.name.common === countryParam ) {
-                console.log(nation.capitalInfo.latlng)
-                getKeyByGeo(nation.capitalInfo.latlng)
+                const coordArr: number[] = nation.capitalInfo.latlng
+                const coordStr: string = `${coordArr[0]},${coordArr[1]}`
+                fetch(`${baseUrl}/weather/${coordStr}`)
+                .then(res=> {
+                    return res
+                })
+                .then(data=> {
+                    return data.json()
+                })
+                .then(json=> {
+                    console.log(json)
+                })
+                .catch(err=> {
+                    console.error(err.message)
+                })
             }
         })
     }
@@ -78,6 +56,7 @@ const ShowPage: FC<Props> = ( { countries, countryCodeObj } ) => {
         })
     }
 
+    
     const getCurrencies = (curr: any) => {
         let currArr: string[] = []
         for ( const key in curr ) {
@@ -91,11 +70,11 @@ const ShowPage: FC<Props> = ( { countries, countryCodeObj } ) => {
    
     useEffect(()=> {
         setCountryParam(countryParam)
-        getLatLong()
+        getWeather()
        
     }, [countryParam])
    
-   console.log(baseUrl)
+  
     return(
         <div className={style.container}>
 
